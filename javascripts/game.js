175fea7aacfe8bc2c8ecee56d2d9bfb9fa363aa2
shupac800 <davidshuman@gmail.com>
2016-03-04 12:27:05 -0600

    kings working

--- javascripts/game.js
+++ javascripts/game.js
@@ -3,23 +3,38 @@
 //let Player = require('./player.js');  // for some reason we crash if Player is required here
 
 let Game = {
-  newMatrix:       [[0,2,0,2,0,2,0,2],
-                    [2,0,2,0,2,0,2,0],
+  newMatrix:       [[0,0,0,2,0,2,0,2],
+                    [2,0,1,0,2,0,2,0],
                     [0,2,0,2,0,2,0,2],
                     [0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0],
                     [1,0,1,0,1,0,1,0],
-                    [0,1,0,1,0,1,0,1],
-                    [1,0,1,0,1,0,1,0]]
+                    [0,1,0,2,0,1,0,1],
+                    [1,0,1,0,0,0,1,0]]
 };
+//   newMatrix:       [[0,0,0,0,0,0,0,2], // game over b/c no moves for P2
+//                     [0,0,0,0,0,0,1,0],
+//                     [0,0,0,0,0,0,0,0],
+//                     [0,0,0,0,0,0,0,0],
+//                     [0,0,0,0,0,0,0,0],
+//                     [1,0,1,0,1,0,1,0],
+//                     [0,1,0,1,0,1,0,1],
+//                     [1,0,1,0,1,0,1,0]]
+// };
 
 Game.doMove = function(moveObj) {
   // first, validate that moveObj defines a valid move
   let error = Game.validate(moveObj);
   $("#error").html(error);
-  console.log("indexOf OK is",error.indexOf("OK"));
 
   if (error.indexOf("OK") >= 0) {  // move is valid
+    // is this piece getting promoted to king?
+    if ((moveObj.player === 1) && (moveObj.dRow === 0)) {
+      moveObj.player = 3; // king player 1!
+    }
+    if ((moveObj.player === 2) && (moveObj.dRow === 7)) {
+      moveObj.player = 4; // king player 2!
+    }
     // move player piece
     Game.matrix[moveObj.oRow][moveObj.oCol] = 0;
     Game.matrix[moveObj.dRow][moveObj.dCol] = moveObj.player;
@@ -35,8 +50,9 @@ Game.validate = function(moveObj) {
       (Math.abs(moveObj.oCol - moveObj.dCol) === 2) ) {
     var jumpRow = (moveObj.oRow + moveObj.dRow) / 2;
     var jumpCol = (moveObj.oCol + moveObj.dCol) / 2;
-    var otherPlayer = moveObj.player === 1 ? 2 : 1;
-    if (Game.matrix[jumpRow][jumpCol] === otherPlayer) {
+    var otherPlayer = ((moveObj.player === 1) || (moveObj.player === 3))? 2 : 1;
+    if ((Game.matrix[jumpRow][jumpCol] === otherPlayer) ||
+        (Game.matrix[jumpRow][jumpCol] === otherPlayer + 2)) {
       Game.matrix[jumpRow][jumpCol] = 0;  // remove jumped piece from board
       return "jump OK!";
     } else {
@@ -46,7 +62,7 @@ Game.validate = function(moveObj) {
   // forward move? only kings can move backward.
   if (((moveObj.player === 1) && (moveObj.dRow > moveObj.oRow ) ) ||
       ((moveObj.player === 2) && (moveObj.dRow < moveObj.oRow ) ) ) {
-    return "can only move forward";
+    return "this piece can only move forward";
   }
   // ordinary move?
   if ((Math.abs(moveObj.oRow - moveObj.dRow) === 1) && 
